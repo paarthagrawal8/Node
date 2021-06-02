c4149a4d0118a8b8ef12ca7de2518b57de4e2d3d
paarth agrawal <paarthagrawal8@gmail.com>
2021-06-02 21:59:27 +0530

    file operation in synchronus way

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const name = "agrawal";
-console.log(name);
+
+// console.log(name);
+
+// operation with files 
+
+const fs = require("fs")
+// creating new files 
+// fs.writeFileSync("read.txt" , "Hello World!")
+// To append new data in file
+// fs.appendFileSync("read.txt" , "This is second line")
+// To read the file
+// const buf_data = fs.readFileSync("read.txt")
+// console.log(buf_data)
+// to read the the file properly
+const buf_data = fs.readFileSync("read.txt" , "utf-8")
+console.log(buf_data)
